d87e8d4b064d5993b585d8ecb611b92cf1984b47
YiguiDing <2449695354@qq.com>
2024-10-09 14:54:38 +0800

    docs: add README.md

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,6 +2,8 @@ import { SimpleProtocalParser } from "./index";
 
 function test() {
   let parser = new SimpleProtocalParser();
+  //   let data = [0x01, 0x02, 0x03];
+  //   let data = [0x7d, 0x55, 0xaa];
   let data = [0x01, 0x02, 0x03, 0x7d, 0x55, 0xaa];
   let encoded = parser.encoder(data);
   let decoded: number[] | null = null;
